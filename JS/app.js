f8c480a099019b2028a0347ea2df16128ed091e6
Hamza Alvi <42583422+hamzaalvi1@users.noreply.github.com>
2020-09-27 00:08:08 +0500

    complete sign-up and login authentication

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+  const loginForm = document.querySelector("#login-form")
+  loginForm.addEventListener("submit",(evt)=>{
+    evt.preventDefault()
+   userLogin(evt)
+  })
+  
+  const userLogin = (evt)=>{
+    const loginUserData = {
+      email : evt.target["email"].value,
+      password: evt.target["password"].value
+    }
+    firebase.auth().signInWithEmailAndPassword(loginUserData.email, loginUserData.password)
+    .then(()=>{
+      window.location.href = "./Html/chat.html"
+
+    })
+    .catch(function(error) {
+      console.log(error)
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+    loginForm.reset()
+  }
--- JS/signup.js
+++ JS/signup.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+
+  const signUpForm = document.querySelector("#signUp-form")
+
+  signUpForm.addEventListener("submit",(evt)=>{
+   evt.preventDefault()
+  signUpUser(evt)
+  })
+
+  const signUpUser = (evt)=>{
+    const signUpUserData = {
+      username : evt.target["username"].value,
+      email : evt.target["email"].value,
+      password: evt.target["password"].value
+    }
+    firebase.auth().createUserWithEmailAndPassword(signUpUserData.email,signUpUserData.password)
+    .then(()=>{
+     console.log("user signUp")     
+    })
+    .catch((error)=>{
+      let errorCode = error.errorCode
+      let errorMessage = error.errorMessage
+      console.log(errorCode,errorMessage)
+    } ) 
+    document.querySelector("#signUp-form").reset()
+    $(document).ready(function(){
+      $('.modal').modal();
+      $('.modal').modal("open");
+
+    });
+   }
